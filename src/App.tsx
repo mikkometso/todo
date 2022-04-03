aae954bd2af23e1b3e9f2f1e06909bd225ae0e4a
Mikko Metso <mikko.metso@sok.fi>
2022-04-03 14:52:23 +0300

    Display draft components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React from 'react'
+import styled from 'styled-components'
+import TodoInput from './components/todoInput/TodoInput'
+import TodoList from './components/todoList/TodoList'
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
 
 const App = () => {
-  return <div className="App">hello world</div>;
-};
+  return (
+    <Wrapper>
+      <h1>Todo app</h1>
+      <TodoInput />
+      <TodoList />
+    </Wrapper>
+  )
+}
 
-export default App;
+export default App
